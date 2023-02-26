cf9bde41298fbc26852a190494fececcec5e5f2a
hieucp <minhieu433@gmail.com>
2023-02-26 15:11:04 +0700

    add home banner

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="header">
+    <RouterLink to="/" class="logo"><img src="../assets/images/taco-logo.png">Hieuhub</RouterLink>
+
     <nav class="nav-bar">
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/">About</RouterLink>
-      <RouterLink to="/">Menu</RouterLink>
-      <RouterLink to="/">Promotions</RouterLink>
-      <RouterLink to="/">Table</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/menu">Menu</RouterLink>
+      <RouterLink to="/promotion">Promotions</RouterLink>
+      <RouterLink to="/table">Table</RouterLink>
     </nav>
+
+    <div class="icon">
+      <div id="menu-btn" class="fas fa-bars menu-btn"></div>
+      
+    </div>
   </div>
 </template>
 
@@ -30,6 +37,20 @@
   padding: 2rem 9%;
 }
 
+/** Header logo */
+.header .logo {
+  font-size: 2.5rem;
+  font-weight: bolder;
+  color: #130f40;
+}
+
+.header .logo img {
+  padding-right: .5rem;
+  color: #27ae60;
+}
+
+/** Header nav-bar */
+
 .header .nav-bar a {
   font-size: 1.7rem;
   margin: 0 1rem;
@@ -40,4 +61,27 @@
   color: #27ae60;
 }
 
+.header .nav-bar a.router-link-exact-active {
+    color: #f38609;
+}
+
+/**Header icons */
+
+.header .icon div {
+  height: 4.5rem;
+  width: 4.5rem;
+  line-height: 4.5rem;
+  font-size: 2rem;
+  background: #f7f7f7;
+  color: #130f40;
+  border-radius: .5rem;
+  margin-left: .3rem;
+  cursor: pointer;
+  text-align: center;
+}
+
+.header .icon div:hover {
+    color: #fff;
+    background: #27ae60 !important;
+}
 </style>
