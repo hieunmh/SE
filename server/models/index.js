41f8ca4b6a5043a51b9f1899273a66f1ebfb8c49
Trần Đức Vinh <97576226+TORIOP23@users.noreply.github.com>
2023-03-31 18:08:50 +0700

    Fix bug

--- server/models/index.js
+++ server/models/index.js
@@ -17,25 +17,10 @@ const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_US
   }
 });
 
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
 db.sequelize = sequelize;
 db.models = {};
-// db.models.User = require('./user')(sequelize, Sequelize.DataTypes);
-// db.models.Product = require('./product')(sequelize, Sequelize.DataTypes);
+db.models.User = require('./user')(sequelize, Sequelize.DataTypes);
+db.models.Product = require('./product')(sequelize, Sequelize.DataTypes);
 
 
 module.exports = db;
