bbe63f9d14ba2adfa3cc2b0512feb4692ebde150
21020382-Quan <qbuiminh5@gmail.com>
2023-04-05 14:30:54 +0700

    sua file sql, them 1 it data

---
+++ feastly-data.sql
@@ -0,0 +1,21 @@
+INSERT INTO product_category VALUES (1, 'Kem', 'Ngon tren tung vi kem', NULL), 
+(2, 'Banh', 'Ngot', NULL),
+(3, 'Đo an', 'Tuy loai', NULL),
+(4, 'Nuoc', '', NULL),
+(5, 'Bia', 'Khong say khong ve', NULL)
+(6, 'Ruou', 'Thuc uong cho ke si tinh', NULL);
+INSERT INTO product_inventory VALUES (1, 122, NULL),
+(2, 211, NULL),
+(3, 222, NULL);
+INSERT INTO discount VALUES (1, 'Giam 10%', 'Giam 10% gia san pham', 0.1, 1, NULL),
+(2, 'Giam 20%', 'Giảm 20% gia san pham', 0.2, 1, NULL),
+(3, 'Giam 30%', 'Giam 30% gia san pham', 0.3, 1, NULL);
+INSERT INTO product VALUES (1, 'KEM OC QUE', 'KEM', 1, 1, '10.000', 1, NULL, 10, NULL)
+(2, 'Banh sinh nhat', 'Banh', 2, 2, '200.000', 2, NULL, 10, NULL);
+INSERT INTO shopping_session VALUES ()
+INSERT INTO cart_item VALUES ()
+INSERT INTO user_payment VALUES ()
+INSERT INTO user_address VALUES ()
+INSERT INTO payment_details VALUES ()
+INSERT INTO order_details VALUES ()
+INSERT INTO order_items VALUES ()
